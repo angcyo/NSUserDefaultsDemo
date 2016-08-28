34cc1c0ada44c20236a18f6a2fc4c00a6e8cd214
angcyo <angcyo@126.com>
2016-08-28 20:45:11 +0800

    2016-08-28

--- NSUserDefaultsDemo/ViewController.swift
+++ NSUserDefaultsDemo/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+		setData()
+		getData()
+	}
 
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 
 }
+
+extension ViewController {
+	func setData() {
+		Defaults.setBool(true, forKey: boolKey)
+		Defaults.setFloat(3.14159265358979323846, forKey: floatKey)
+		Defaults.setDouble(1.41414249812398467213441255598776, forKey: doubleKey)
+		Defaults.setObject("Json String ...", forKey: objectKey)
+		Defaults.setInteger(19991111, forKey: intKey)
+	}
+
+	func getData() {
+		print("\(Defaults.boolForKey(boolKey))")
+		print("\(Defaults.floatForKey(floatKey))")
+		print("\(Defaults.doubleForKey(doubleKey))")
+		print("\(Defaults.objectForKey(objectKey))")
+		print("\(Defaults.integerForKey(intKey))")
+	}
+}
+
+let Defaults = NSUserDefaults.standardUserDefaults()
+let boolKey = "boolKey"
+let floatKey = "floatKey"
+let doubleKey = "doubleKey"
+let objectKey = "objectKey"
+let intKey = "intKey"
 
